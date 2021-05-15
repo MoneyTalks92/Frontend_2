b38471610a5ef0406e93c9a49bc2697fe0e99098
Ferenc Bujtas <anothercp3@gmail.com>
2021-05-15 16:22:17 +0200

    dog app

---
+++ dog-browser-start/src/app.js
@@ -0,0 +1,6 @@
+import './css/style.css';
+import './index.html';
+import SearchImage from './searchImageComponent/searchImageComponent.js';
+
+const mySearchImage = new SearchImage();
+mySearchImage.render();

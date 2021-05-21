7811f7494302d9fc5b11baabfad5c7f65f4bed5c
Ferenc Bujtas <anothercp3@gmail.com>
2021-05-21 18:23:50 +0200

    chat browser

---
+++ chat-starter/src/db_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyAcMCfc9-pZK_-qaYxr_379S37wDax9fCA',
+  authDomain: 'chatapp-wsuf.firebaseapp.com',
+  databaseURL: 'chatapp-wsuf.firebaseio.com',
+  projectId: 'chatapp-wsuf',
+  storageBucket: 'chatapp-wsuf.appspot.com',
+  messagingSenderId: '103291939596'
+};
+
+export default config;
